568fdc7566aab1f76d1578dae06bedaf3d1fe4a1
Taher Maimoon <tmaimoon18@gmail.com>
2025-01-30 22:25:17 +0530

    Updated Category model

--- Models/Category.Model.cs
+++ Models/Category.Model.cs
@@ -4,5 +4,10 @@ namespace FundTransfer
     {
         public int Id { get; set; }
         public string? CategoryName { get; set; }
+        public int UserId { get; set; }
+        public int TypeId { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public bool IsActive { get; set; }
     }
 }
